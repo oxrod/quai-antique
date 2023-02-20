8dd5b3914315ab8870a738f6be91d37ada78e1fd
Oxrod <oxrodo@gmail.com>
2023-02-21 00:29:52 +0100

    Add back AdminFixture

---
+++ src/DataFixtures/AdminFixture.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AdminFixture extends \Doctrine\Bundle\FixturesBundle\Fixture
+{
+    private UserPasswordHasherInterface $userPasswordHasher;
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        $dummyAdmin = new User();
+        $dummyAdmin->setCutleryNumber(0)
+            ->setPassword(
+                $this->userPasswordHasher->hashPassword(
+                    $dummyAdmin,
+                    'admin'
+                )
+            )
+            ->setRoles(["ROLE_ADMIN"]);
+    }
+}
