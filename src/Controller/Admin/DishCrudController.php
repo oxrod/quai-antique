6381a4262d0a0653d1c75061f53a403a1d2fd2da
Oxrod <oxrodo@gmail.com>
2023-02-22 14:20:26 +0100

    Remove dish crud category field

--- src/Controller/Admin/DishCrudController.php
+++ src/Controller/Admin/DishCrudController.php
@@ -6,11 +6,7 @@ use App\Entity\Dish;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class DishCrudController extends AbstractCrudController
@@ -21,16 +17,16 @@ class DishCrudController extends AbstractCrudController
     }
 
 
-//    public function configureFields(string $pageName): iterable
-//    {
-//        return [
-//            TextField::new('title'),
-//            TextField::new('description'),
-//            TextField::new('price'),
-//            AssociationField::new('category'),
-//            TextField::new('image'),
-//            BooleanField::new('isFeatured')
-//        ];
-//    }
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('title');
+        yield TextField::new('description');
+        yield TextField::new('price');
+        yield ImageField::new('image')
+            ->setBasePath('uploads/images')
+            ->setUploadDir('public/uploads/images');
+        yield BooleanField::new('isFeatured');
+
+    }
 
 }
